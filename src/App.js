25519fcd9f0bfcf02c729e0b6c0aee44dc81c06e
elizabethjohnson2 <ej86391@gmail.com>
2023-02-15 19:41:27 +0530

    React LefterSection

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from"react";
+import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
 
 import Nav from"./components/includes/Nav";
 
+
 function App() {
-  return <Nav/>
+  return (
+  <Router>
+    <Nav/>
+    
+    <Routes>
+  <Route/>
+  </Routes>
+  </Router>
+  );
+  
 }
 
 export default App;
--- src/components/includes/Nav.js
+++ src/components/includes/Nav.js
@@ -1,47 +1,71 @@
-import React from "react";
+import React,{useState} from "react";
 import styled from "styled-components";
 
+
+
  export default function Nav(){
-    // const [persons,setPersons]= useState([
-    //     {
-    //         id:1,
-    //         name:'Griffin Wooldridge',
-    //         age:20,
-    //         image:require("../../assets/images/Ellipse4.png")
-    //     },
-    //     {
-    //         id:2,
-    //         name:'Hester Hogan',
-    //         age:23,
-    //         image:require("../../assets/images/Ellipse3.png")
-    //     },
-    //     {
-    //         id:3,
-    //         name:'Danny Winget',
-    //         age:25,
-    //         image:require("../../assets/images/Ellipse2.png")
-    //     },
-    //     {
-    //         id:4,
-    //         name:'Joshua Vergara',
-    //         age:23,
-    //         image:require("../../assets/images/Ellipse1.png")
-    //     },
-    //     {
-    //         id:5,
-    //         name:'Jon Rettinger',
-    //         age:20,
-    //         image:require("../assets/images/Ellipse5.png")
-    //     },
-    // ])
-    
+    const [tags,setTags]= useState([
+        {
+            id:1,
+            name:'Dashboard',
+            
+            image:require("../../assets/images/Property 1=dashboard.svg"),
+        },
+        {
+            id:2,
+            name:'All Files',
+            image:require("../../assets/images/Property 1=all.svg"),
+        },
+        {
+            id:3,
+            name:'Shared',
+            image:require("../../assets/images/Property 1=share.svg"),
+        },
+        {
+            id:4,
+            name:'favourites',
+           
+            image:require("../../assets/images/Property 1=like.svg")
+        },
+        {
+            id:5,
+            name:'Recent',
+            image:require("../../assets/images/Property 1=unread.svg"),
 
+        },
+        {
+            id:6,
+            name:'Request',
+            image:require("../../assets/images/Property 1=reduce.svg"),
+            
+        },
+    ])
+    const renderTags =()=>{
+        return tags.map((tag)=>(
+            <LinkTag>
+                <LinkImage src={tag.image}/>
+                <LinkName>{tag.name}</LinkName>
+                </LinkTag>
 
+        ))
+    }
     return (
+        
         <MainContainer>
-     
+            <Leftcontainer>
+                <TagContainer>
+                    <TagName>a</TagName>
+                </TagContainer>
+                <Heading>arifeen</Heading>
+                <ListTagItem>
+                    {renderTags}
+                </ListTagItem>
+                
+            </Leftcontainer>
+            
         <HeaderContainer>
             <Left>
+            <LineTag></LineTag>
                 <LogoImage src={require("../../assets/images/Property 1=serach.svg").default} alt="logo"/>
                 <StateImage src={require("../../assets/images/Component 429.svg").default} alt="Image"/>
                 <ImageTag>
@@ -59,20 +83,47 @@ import styled from "styled-components";
                 <TagImage src={require("../../assets/images/Property 1=down.svg").default} alt="arrow"/>
             </Right>
         </HeaderContainer>
-        <LeftContainer>
-
-
-        </LeftContainer>
+        
+       
+       
        
         </MainContainer>
      
     )
 }
-    const MainContainer=styled.div``;
+    const MainContainer=styled.div`
+     
+     display:flex;
+     background-color:#212236;
+    
+
+   margin-left: auto;
+    
+   
+    `;
+      const Leftcontainer=styled.div`
+      width:11%;
+      height:500px;
+      background-color:#1B1C30;
+      `;
+      const TagContainer=styled.div``;
+      const TagName=styled.button`
+      background-color:#84E9F4;
+      margin-top:20px;
+      border-radius: 30%;`;
+      const Heading=styled.h1``;
+      const ListTagItem=styled.div``;
+      const LinkTag=styled.div``;
+      const LinkImage=styled.img``;
+      const LinkName=styled.p``;
     const HeaderContainer=styled.div`
+    width:80%;
+    
+   
     display:flex;
     justify-content:space-between;
     margin-left:10%;
+    padding:10px;
      
     
     `;
@@ -83,61 +134,85 @@ import styled from "styled-components";
     justify-content: space-between;
    
     `;
-    
+    const LineTag=styled.div`
+    border-bottom:3px solid #000;
+    `;
     const LogoImage=styled.img`
-     width: 14%;
+         width: 5%;
     display: block;
-    margin-right: 100px;
+    margin-right: 445px;
+    margin-bottom: 393px;
    
    
    `;
    const ImageTag=styled.div`
    display:flex;
-   width:30px;
+ 
    margin:0 auto;
   
    `;
     const StateImage=styled.img`
-     width: 18%;  
+     width: 5%;  
     display: flex;
     border-radius: 50%;
-    margin-left: 359px;
+    margin-right: 77px;
+    margin-bottom: 426px;
    `;
     const StateName=styled.p`
     font-size:10px;
+    margin-right: 26px;
+    margin-bottom: 400px;
+    color:#727488;
+
+    margin-left: -59px;
+    
     `;
     const ArrowImage=styled.img`
+     margin-right:10px;
+    display:flex;
+    width: 24px;
+    margin-bottom: 424px;
    
     `;
     
     const ArrowName=styled.p`
-   margin-right: -57px;
+           margin-right: 141px;
+       color:#727488;
+       margin-top: 10px;
     `;
     const Middle=styled.div`
-    width: 10%;
+    width: 8%;
+    padding-right:20px;
    
     display: flex;
     `;
     const PropertyImage=styled.img`
-     width:20%;
-    display: flex;`;
+     width:30%;
+    display: flex;
+    
+    margin-bottom: 425px;`;
     const BellImage=styled.img`
-     width:20%;
-    display: flex;`;
+     width:30%;
+    display: flex;
+    margin-bottom: 425px;`;
     const Right=styled.div`
     width: 10%;
+    padding-right:20px;
    
     display: flex;
     justify-content: space-between;`;
     const FrameImage=styled.img`
-      width:44%;
+      width:50%;
     display: flex;
     border-radius: 50%;
+    margin-bottom: 425px;
     `;
     const TagImage=styled.img`
-     width:10%;
+     width:20%;
     display: flex;
     margin-right: 62px;
+    margin-bottom:400px;
     `;
-    const LeftContainer=styled.div``;
+  
+   
 
